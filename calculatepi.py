20959fc1948407878faf22b5f724e4d0791cc3cd
Funjando <andojuuichi@gmail.com>
2016-02-02 14:12:33 -0500

    Updated from Brython Server: 2/2/2016 2:12:36 PM

--- calculatepi.py
+++ calculatepi.py
@@ -1,7 +1,7 @@
 """
 calculatepi.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Andreas Moberg
+Credit: Dan Wilson
 Assignment:
 
 Write and submit a Python program that computes an approximate value of π by calculating the following sum:
